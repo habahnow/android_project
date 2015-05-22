6daffb407ab7cb065069c4ae3e682ade7abbb904
daniel <danielsoto03@gmail.com>
2015-05-22 15:17:06 -0700

    added base code added Alarm_activity and layout

---
+++ app/src/main/java/com/example/danny/android_project/Alarm_activity.java
@@ -0,0 +1,56 @@
+package com.example.danny.android_project;
+
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+
+import java.util.Calendar;
+
+/**
+ * Created by danny on 5/22/15.
+ */
+public class Alarm_activity extends ActionBarActivity{
+    private MediaPlayer mp;
+
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_alarm_activity);
+
+        Uri notification = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM);
+        mp = MediaPlayer.create(getApplicationContext(), notification);
+        mp.start();
+
+    }
+
+    public void dismiss(View v){
+        mp.stop();
+        mp.release();
+
+
+
+    }
+
+    public void snooze(View v){
+        mp.stop();
+        mp.release();
+
+        Intent i = new Intent(getApplicationContext(), Alarm_activity.class);
+        PendingIntent pi = PendingIntent.getActivity(getApplicationContext(),333,i,
+                PendingIntent.FLAG_CANCEL_CURRENT);
+
+        //getting current time and add 5 seconds in it
+        Calendar cal = Calendar.getInstance();
+        cal.add(Calendar.SECOND, 2);
+
+        //registering our pending intent with alarmmanager
+        AlarmManager am = (AlarmManager) getSystemService(ALARM_SERVICE);
+        am.set(AlarmManager.RTC_WAKEUP, cal.getTimeInMillis(), pi);
+
+    }
+}
---
+++ app/src/main/java/com/example/danny/android_project/Main_activity.java
@@ -0,0 +1,54 @@
+package com.example.danny.android_project;
+
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import java.util.Calendar;
+
+
+public class Main_activity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity);
+        Intent i = new Intent(getApplicationContext(), Alarm_activity.class);
+        PendingIntent pi = PendingIntent.getActivity(getApplicationContext(),3333,i,
+                PendingIntent.FLAG_CANCEL_CURRENT);
+
+        //getting current time and add 5 seconds in it
+        Calendar cal = Calendar.getInstance();
+        cal.add(Calendar.SECOND, 3);
+
+        //registering our pending intent with alarmmanager
+        AlarmManager am = (AlarmManager) getSystemService(ALARM_SERVICE);
+        am.set(AlarmManager.RTC_WAKEUP,cal.getTimeInMillis(), pi);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_alarm_activity, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
