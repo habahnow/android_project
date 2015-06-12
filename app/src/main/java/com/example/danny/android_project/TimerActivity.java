7a2927bb87550d4a2b5a772d95f1dd5467b79b85
daniel <danielsoto03@gmail.com>
2015-06-12 12:13:35 -0700

    Started adding in the files for the timer.

---
+++ app/src/main/java/com/example/danny/android_project/TimerActivity.java
@@ -0,0 +1,20 @@
+package com.example.danny.android_project;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.Button;
+
+/**
+ * Created by danny on 6/12/15.
+ */
+public class TimerActivity extends Activity {
+
+    private Button weatherBut, todoBut;
+
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.timer_layout);
+        weatherBut = (Button) findViewById(R.id.weatherBut);
+        todoBut = (Button) findViewById(R.id.To_DoBut);
+    }
+}
