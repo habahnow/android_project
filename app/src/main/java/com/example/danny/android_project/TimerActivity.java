6751c72f717c20c96c16c16613861d9a1525f9dd
daniel <danielsoto03@gmail.com>
2015-06-12 12:59:14 -0700

    implemented start and stop timers

--- app/src/main/java/com/example/danny/android_project/TimerActivity.java
+++ app/src/main/java/com/example/danny/android_project/TimerActivity.java
@@ -1,17 +1,23 @@
 package com.example.danny.android_project;
 
 import android.app.Activity;
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+
+import java.util.Calendar;
 
 /**
  * Created by danny on 6/12/15.
  */
 public class TimerActivity extends Activity {
 
-    private Button weatherBut, todoBut;
+    private Button weatherBut, todoBut, alarmBut;
     private Button start;
     private Button stop;
 
@@ -32,10 +38,31 @@ public class TimerActivity extends Activity {
     }
 
     public void startTimer(View v){
+        int hour = Integer.parseInt(hourEditText.getText().toString());
+        int minute = Integer.parseInt(minuteEditText.getText().toString());
+        int second= Integer.parseInt(secondEditText.getText().toString());
+
+        int totalSeconds = second;
+
+        totalSeconds +=  minute * 60;
+        totalSeconds += hour * 60 * 60;
+
+        Calendar cal = Calendar.getInstance();
+
+        cal.add(Calendar.SECOND, totalSeconds);
+
+        Intent intent = new Intent(this, AlarmActivity.class);
+        PendingIntent pi= PendingIntent.getBroadcast(this, 0, intent, 0);
+        AlarmManager am = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+        am.set(AlarmManager.RTC_WAKEUP, cal.getTimeInMillis() , pi);
 
     }
 
     public void stopTimer(View v){
+        Intent intent = new Intent(this, AlarmActivity.class);
+        PendingIntent pi= PendingIntent.getBroadcast(this, 0, intent, 0);
+
+        AlarmManager am = (AlarmManager) getSystemService(Con)
 
     }
 }
